bd039cdbde571ca5ed9a347fa6668b474a4fc9b8
de-fer <dm.chuenkov@gmail.com>
2026-02-21 20:16:41 +0300

    home manager

--- flake.nix
+++ flake.nix
@@ -1,19 +1,35 @@
 {
+
   description = "My NixOS configuration";
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     lib = nixpkgs.lib;
-  in {
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in{
     nixosConfigurations = {
       laptop-pavilion = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
 	modules = [ ./configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      de-fer = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+	modules = [ ./home/de-fer.nix ];
       };
     };
   };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
 }
---
+++ home/de-fer.nix
@@ -0,0 +1,27 @@
+# home/de-fer.nix
+{ config, pkgs, lib, ...}:
+
+{
+  programs.home-manager.enable = true;
+
+  # Basic
+  home.username = "de-fer";
+  home.homeDirectory = "/home/de-fer";
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    # TTY
+    neovim
+    git
+    lazygit
+
+    # Programs
+    firefox
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "de-fer";
+    userEmail = "dm.chuenkov@gmail.com";
+  };
+}
