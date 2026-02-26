43bc0df248db07de720d1bd630b1563c124de04b
de-fer <dm.chuenkov@gmail.com>
2026-02-26 16:31:43 +0300

    notification with swaync

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -14,6 +14,7 @@
 
   environment.systemPackages = with pkgs; [
     waybar       # панелька
+    swaynotificationcenter # уведомления
     hyprpaper    # обои
     hyprshot     # скриншоты
     hyprlock     # экран блокировки
