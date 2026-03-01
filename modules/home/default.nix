963d7b34eaa3857b21fcd9bb77948494fc42f489
de-fer <dm.chuenkov@gmail.com>
2026-03-01 11:14:39 +0300

    minecraft

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -6,6 +6,7 @@
     ./zsh/defaul.nix
     ./kitty.nix
     ./nixcord.nix
+    ./prismlauncher.nix
     ./zen-browser.nix
   ];
 }
---
+++ modules/home/prismlauncher.nix
@@ -0,0 +1,5 @@
+{ pkgs, home, ... }:
+
+{
+  home.packages = with pkgs; [ prismlauncher ];
+}
